6d398ef4cddf1cd03591a9c92e4471d9be7d07f5
ThisisHappyEL <1379This_is_HappyEL@mail.ru>
2023-12-21 18:40:39 +0700

    Добавил комментарий для удобства

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const RockAndScissorsAndPaperGame = () => {
   const compChoise = Math.floor(Math.random() * chooseModificator);
   console.log('Добро пожаловать в игру "Камень, Ножницы, Бумага"!');
   const youChoise = readlineSync.keyInSelect(itemToGame, 'Выберите ваш предмет:');
+  // console.log('Проверка выбора игроком', youChoise); Если нужно проверить индекс игрока
   if (youChoise < 0 || youChoise > 2) {
     return console.log('Напишите node.index.js ещё раз и на этот раз выберите предмет под порядковым номером 1, 2 или 3, пожалуйста.');
   }
   console.log(`Вы выбрали ${itemToGame[youChoise]}`);
   console.log(`Компьютер выбирает: ${itemToGame[compChoise]}`);
+  // console.log('Проверка генерации выбора', compChoise);  Если нужно проверить индекс компа
   switch (true) {
     case youChoise === compChoise:
       return console.log('Результат: Ничья!. Чтобы на этот раз попробовать победить наберите "node index.js" в консоли ;)');
